4b0e241fab26bd85e2fb15853eb4ab680232c165
Aidan Timson <aidan@timmo.dev>
2024-08-24 16:32:29 +0100

    Setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,19 @@
 import "~/styles/globals.css";
 
-import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 import { TRPCReactProvider } from "~/trpc/react";
 import { ThemeProvider } from "~/components/theme-provider";
+import { HydrateClient } from "~/trpc/server";
+import { ModeToggle } from "~/components/theme-toggle";
 
 export const metadata: Metadata = {
   title: "Notes",
@@ -16,17 +25,37 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <TRPCReactProvider>{children}</TRPCReactProvider>
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" className={`${GeistSans.variable}`}>
+        <body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <TRPCReactProvider>
+              <HydrateClient>
+                <header className="flex w-full items-center justify-between bg-gradient-to-r from-[#2e026d] to-[#15162c] p-4">
+                  <h1 className="text-2xl font-bold">Notes</h1>
+                  <SignedIn>
+                    <UserButton />
+                  </SignedIn>
+                  <SignedOut>
+                    <SignInButton />
+                  </SignedOut>
+                  <ModeToggle />
+                </header>
+                <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+                  <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+                    {children}
+                  </div>
+                </main>
+              </HydrateClient>
+            </TRPCReactProvider>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
