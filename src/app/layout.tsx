013580beaae264a211bc9ce2c5fc43f44ae4a0db
Aidan Timson <aidan@timmo.dev>
2024-08-24 16:26:13 +0100

    Setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import "~/styles/globals.css";
+
+import { GeistSans } from "geist/font/sans";
+import { type Metadata } from "next";
+
+import { TRPCReactProvider } from "~/trpc/react";
+import { ThemeProvider } from "~/components/theme-provider";
+
+export const metadata: Metadata = {
+  title: "Notes",
+  description: "A simple notes app",
+  icons: [{ rel: "icon", url: "/icon" }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${GeistSans.variable}`}>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <TRPCReactProvider>{children}</TRPCReactProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
