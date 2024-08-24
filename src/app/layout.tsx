7326709d8eb64b732bfa783ad19c1874e04a25d7
Aidan Timson <aidan@timmo.dev>
2024-08-24 16:56:25 +0100

    Setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,20 +2,13 @@ import "~/styles/globals.css";
 
 import { type Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
-import { dark } from '@clerk/themes'
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 import { TRPCReactProvider } from "~/trpc/react";
 import { ThemeProvider } from "~/components/theme-provider";
 import { HydrateClient } from "~/trpc/server";
-import { ModeToggle } from "~/components/theme-toggle";
-import { Button } from "~/components/ui/button";
+import Header from "~/components/header";
 
 export const metadata: Metadata = {
   title: "Notes",
@@ -42,21 +35,7 @@ export default function RootLayout({
           >
             <TRPCReactProvider>
               <HydrateClient>
-                <header className="flex w-full items-center justify-between gap-2 p-4">
-                  <h1 className="flex-1 text-2xl font-bold">Notes</h1>
-                  <ModeToggle />
-                  <SignedIn>
-                    <UserButton />
-                  </SignedIn>
-                  <SignedOut>
-                    <Button
-                      className="transform rounded-md bg-violet-800 bg-opacity-80 px-4 py-2 text-white shadow-lg drop-shadow-2xl transition duration-300 hover:scale-105 hover:bg-violet-800"
-                      variant="outline"
-                    >
-                      <SignInButton mode="modal" />
-                    </Button>
-                  </SignedOut>
-                </header>
+                <Header />
                 <main className="flex min-h-screen flex-col items-center justify-center">
                   <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
                     {children}
---
+++ src/components/header.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
+
+import { ModeToggle } from "~/components/theme-toggle";
+
+export default function Header() {
+  const pathname = usePathname();
+
+  const title = useMemo<string>(() => {
+    switch (pathname) {
+      case "/settings":
+        return "Settings";
+      default:
+        return "Notes";
+    }
+  }, [pathname]);
+
+  return (
+    <header className="flex w-full items-center justify-between gap-2 p-4">
+      <h1 className="flex-1 text-2xl font-bold">{title}</h1>
+      <ModeToggle />
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        <div className="transform rounded-md bg-violet-800 bg-opacity-80 px-4 py-2 text-white shadow-lg drop-shadow-2xl transition duration-300 hover:scale-105 hover:bg-violet-800">
+          <SignInButton mode="modal" />
+        </div>
+      </SignedOut>
+    </header>
+  );
+}
