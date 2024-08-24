2302c58d49f5a568b89725f5213e183ee863d23f
Aidan Timson <aidan@timmo.dev>
2024-08-24 19:59:15 +0100

    Fix SVG

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,13 +45,13 @@ export default async function Home() {
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
                 className="size-32"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M12 4.5v15m7.5-7.5h-15"
                 />
               </svg>
