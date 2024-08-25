1757ede070c713b36720bf9b441a25a82a8052a9
Aidan Timson <aidan@timmo.dev>
2024-08-25 20:27:08 +0100

    Order in asc not desc

--- src/server/api/routers/page.ts
+++ src/server/api/routers/page.ts
@@ -26,7 +26,7 @@ export const pageRouter = createTRPCRouter({
     .query(async ({ ctx, input }) => {
       const post = await ctx.db.query.pages.findMany({
         where: (pages, { eq }) => eq(pages.notebookId, input.notebookId),
-        orderBy: (pages, { desc }) => [desc(pages.title)],
+        orderBy: (pages, { asc }) => [asc(pages.title)],
       });
       return post ?? null;
     }),
