7b5bea54dad5b1e8c1b57e4dcdd6928a8f45d81f
Aidan Timson <aidan@timmo.dev>
2024-09-01 11:28:46 +0100

    Split item

--- src/app/notebook/[notebookId]/_components/navigation.tsx
+++ src/app/notebook/[notebookId]/_components/navigation.tsx
@@ -1,6 +1,7 @@
 "use client";
-import { NotebookText, Plus } from "lucide-react";
+import { NotebookText, Plus, Trash } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { Button } from "~/components/ui/button";
 
 import {
   Command,
@@ -29,8 +30,6 @@ export function Navigation({
   selectedPage: number | null;
   onNewPage: () => void;
 }) {
-  const router = useRouter();
-
   return (
     <Command className="flex max-h-[72vh] w-[248px] flex-col rounded-b-lg rounded-t-xl border">
       <CommandInput placeholder="Search" />
@@ -42,21 +41,47 @@ export function Navigation({
             <span>New Page</span>
           </CommandItem>
           {pages.map((page) => (
-            <CommandItem
+            <Item
               key={page.id}
-              className={`${
-                selectedPage === page.id ? "bg-accent/50" : ""
-              } hover:bg-accent-hover`}
-              onSelect={() => {
-                router.push(`/notebook/${notebookId}/page/${page.id}`);
-              }}
-            >
-              <NotebookText className="mr-2 h-4 w-4" />
-              <span>{page.title}</span>
-            </CommandItem>
+              notebookId={notebookId}
+              page={page}
+              selected={selectedPage === page.id}
+            />
           ))}
         </CommandGroup>
       </CommandList>
     </Command>
+  );
+}
+
+function Item({
+  notebookId,
+  page,
+  selected,
+}: {
+  notebookId: number;
+  page: {
+    id: number;
+    title: string | null;
+    createdAt: Date;
+    updatedAt: Date | null;
+    content: string | null;
+    notebookId: number;
+  };
+  selected: boolean;
+}) {
+  const router = useRouter();
+
+  return (
+    <CommandItem
+      key={page.id}
+      className={`${selected ? "bg-accent/50" : ""} hover:bg-accent-hover`}
+      onSelect={() => {
+        router.push(`/notebook/${notebookId}/page/${page.id}`);
+      }}
+    >
+      <NotebookText className="mr-2 h-4 w-4" />
+      <span>{page.title}</span>
+    </CommandItem>
   );
 }
