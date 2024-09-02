311d97e63d4b2d5498681aed470686c509e47b39
Aidan Timson <aidan@timmo.dev>
2024-09-02 20:15:01 +0100

    Disable hydration warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
         baseTheme: dark,
       }}
     >
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body
           className={cn(
             "min-h-screen bg-background font-sans antialiased",
